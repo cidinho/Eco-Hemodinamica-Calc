7abb5e1cb540c0a6d81c84d744bf00ad7bc1c64c
Alcides Bezerra <cidinho.cide@gmail.com>
2022-03-07 00:46:30 -0300

    bloco de variáveis clínicas

--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
-<<<<<<< HEAD
 import './registerServiceWorker'
-=======
->>>>>>> c5ae6e51cc1b61f5b9fb539c08f111b1dc843278
 
 Vue.config.productionTip = false
 
